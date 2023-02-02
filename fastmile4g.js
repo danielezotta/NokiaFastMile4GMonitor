26f7dfecb52acedaf8d4da2f1d6f29f3ae8ef0c4
Daniele <danielezotta@gmail.com>
2023-02-02 21:24:50 +0100

    Added lower night monitoring frequency

--- fastmile4g.js
+++ fastmile4g.js
@@ -1,58 +1,66 @@
 const https = require("https");
 const HTMLParser = require('node-html-parser');
+var nightTimeout;
 
 function saveData(ip, db) {
 
-  setInterval(() => {
+  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
-    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+  https.get("https://" + ip + "/status.php", res => {
 
-    https.get("https://" + ip + "/status.php", res => {
-  
-      var page = [];
-  
-      res.on('data', c => {
-        page.push(c);
-      });
-  
-      res.on('end', () => {
-        const data = Buffer.concat(page).toString();
-        var primaryData = getPrimaryData(data);
-        var secondaryData = getSecondaryData(data);
-        
-        prevRecording = db.get("signals").orderBy("time", "desc").value()[0];
-        if (typeof prevRecording !== 'undefined' && Object.keys(prevRecording.primary).length !== 0) {
-          if (Object.keys(primaryData).length !== 0 && prevRecording.primary.band != primaryData.band) {
-            db.get("changes").push({ "time": Date.now(), "primary" : prevRecording.primary.band + " -> " + primaryData.band }).write();
-          } else if (Object.keys(primaryData).length === 0) {
-            db.get("changes").push({ "time": Date.now(), "primary" : prevRecording.primary.band + " -> " + "No data" }).write();
-          }
-          if (secondaryData.length !== 0 && prevRecording.secondary.length != secondaryData.length) {
-            db.get("changes").push({ "time": Date.now(), "secondary" : prevRecording.secondary.map(s => s.band) + " -> " + secondaryData.map(s => s.band) }).write();
-          } else if (secondaryData.length === 0 && prevRecording.secondary.length != 0) {
-            db.get("changes").push({ "time": Date.now(), "secondary" : prevRecording.secondary.map(s => s.band) + " -> " + "No aggregation" }).write();
-          }
-        } else if (typeof prevRecording !== 'undefined' && Object.keys(prevRecording.primary).length === 0) {
-          if (Object.keys(primaryData).length !== 0) {
-            db.get("changes").push({ "time": Date.now(), "primary" : "No data" + " -> " + primaryData.band }).write();
-          }
-        }
+    var page = [];
 
-        db.get("signals").push({ "time": Date.now(), "primary" : primaryData, "secondary" : secondaryData }).write();
-        if (db.get('signals').value().length > 30) {
-          db.get('signals').remove({ "time": db.get('signals').value()[0].time }).write();
-        }
-        if (db.get('changes').value().length > 30) {
-          db.get('changes').remove({ "time": db.get('changes').value()[0].time }).write();
-        }
-      });
-  
-    }).on('error', function(err) {
-      console.log(err);
+    res.on('data', c => {
+      page.push(c);
     });
 
-  }, db.get("millis"));
+    res.on('end', () => {
+      const data = Buffer.concat(page).toString();
+      var primaryData = getPrimaryData(data);
+      var secondaryData = getSecondaryData(data);
+      
+      prevRecording = db.get("signals").orderBy("time", "desc").value()[0];
+      prevPrevRecording = db.get("signals").orderBy("time", "desc").value()[1];
 
+      if (typeof prevRecording !== 'undefined' && Object.keys(prevRecording.primary).length !== 0) {
+        if (Object.keys(primaryData).length !== 0 && prevRecording.primary.band != primaryData.band) {
+          db.get("changes").push({ "time": Date.now(), "primary" : prevRecording.primary.band + " -> " + primaryData.band }).write();
+        } else if (Object.keys(primaryData).length === 0) {
+          db.get("changes").push({ "time": Date.now(), "primary" : prevRecording.primary.band + " -> " + "No data" }).write();
+        }
+
+        if (secondaryData.length !== 0 && prevRecording.secondary.length != secondaryData.length) {
+          db.get("changes").push({ "time": Date.now(), "secondary" : prevRecording.secondary.map(s => s.band) + " -> " + secondaryData.map(s => s.band) }).write();
+        } else if (secondaryData.length === 0 && prevRecording.secondary.length != 0) {
+          db.get("changes").push({ "time": Date.now(), "secondary" : prevRecording.secondary.map(s => s.band) + " -> " + "No aggregation" }).write();
+        }
+        
+      } else if (typeof prevRecording !== 'undefined' && Object.keys(prevRecording.primary).length === 0) {
+        if (Object.keys(primaryData).length !== 0) {
+          db.get("changes").push({ "time": Date.now(), "primary" : "No data" + " -> " + primaryData.band }).write();
+        }
+      }
+
+      db.get("signals").push({ "time": Date.now(), "primary" : primaryData, "secondary" : secondaryData }).write();
+      if (db.get('signals').value().length > 30) {
+        db.get('signals').remove({ "time": db.get('signals').value()[0].time }).write();
+      }
+      if (db.get('changes').value().length > 30) {
+        db.get('changes').remove({ "time": db.get('changes').value()[0].time }).write();
+      }
+    });
+
+  }).on('error', function(err) {
+    console.log(err);
+  });
+
+}
+
+function setTimer(ip, db) {
+  setTimeout(() => {
+    saveData(ip, db);
+    setTimeout(() => { setTimer(ip, db); }, (db.get("night").value().enabled) ? db.get("night").value().millis : db.get("millis"));
+  }, (db.get("night").value().enabled) ? db.get("night").value().millis : db.get("millis") );
 }
 
 function getSecondaryData(html) {
@@ -106,6 +114,55 @@ function getPrimaryData(html) {
   }
 }
 
+function setNight(b, db) {
+  var night = db.get("night").value();
+  night.enabled = b;
+  db.set("night", night).write();
+
+  setNightTimer(db);
+}
+
+function setNightTimer(db) {
+  var dateNow = new Date();
+  
+  var condition = false;  
+  if (db.get("night").value().start > db.get("night").value().end) {
+    condition = (dateNow.getHours() >= db.get("night").value().start || dateNow.getHours() < db.get("night").value().end);
+  } else {
+    condition = (dateNow.getHours() >= db.get("night").value().start && dateNow.getHours() < db.get("night").value().end);
+  }
+
+  if (condition) {
+    var dateTimer = new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate(), db.get("night").value().end);
+    nightTimeout = setTimeout(() => {
+      setNight(false, db);
+    }, dateTimer.getTime() - dateNow.getTime() + 60000);
+  } else {
+    var dateTimer = new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate(), db.get("night").value().start);
+    if (dateNow.getHours() > db.get("night").value().start) {
+      dateTimer = new Date(dateTimer.getTime() + 86400000);
+    }
+    nightTimeout = setTimeout(() => {
+      setNight(true, db);
+    }, dateTimer.getTime() - dateNow.getTime() + 60000);
+  }
+}
+
+function setNightStartup(db) {
+  var dateNow = new Date();
+
+  var condition = false;  
+  if (db.get("night").value().start > db.get("night").value().end) {
+    condition = (dateNow.getHours() >= db.get("night").value().start || dateNow.getHours() < db.get("night").value().end);
+  } else {
+    condition = (dateNow.getHours() >= db.get("night").value().start && dateNow.getHours() < db.get("night").value().end);
+  }
+
+  var night = db.get("night").value();
+  night.enabled = condition;
+  db.set("night", night).write();
+}
+
 exports.now = (ip, response) => {
   
   process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
@@ -130,7 +187,13 @@ exports.now = (ip, response) => {
   });
 }
 
-exports.timer = (ip, db, response) => {
-  saveData(ip, db);
-  // response.send({ "delay": delay + " ms", "ip": ip });
+exports.timer = (ip, db) => {
+  setTimer(ip, db);
+  setNightStartup(db);
+  setNightTimer(db);
+}
+
+exports.reloadNightTimer = (db) => {
+  clearTimeout(nightTimeout);
+  setNightTimer(db);
 }
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fastmile = require("./fastmile4g.js");
 const adapter = new FileSync('db.json');
 const db = low(adapter);
 
-db.defaults({ ip: "", millis: 120000, signals: [], changes: [] }).write();
+db.defaults({ ip: "", millis: 120000, signals: [], changes: [], night: { enabled: true, start: 23, end: 7, millis: 600000 } }).write();
 
 var app = express();
 app.use(express.json());
@@ -30,8 +30,19 @@ app.get("/ip", (req, res) => {
 })
 
 app.post("/history/delay", (req, res) => {
-  db.set("millis", req.body.delay).write();
+  db.set("millis", (req.body.delay == 0)? 120000 : req.body.delay).write();
   res.json({ "delay": req.body.delay });
+})
+
+app.post("/history/night", (req, res) => {
+  var night = {};
+  night.enabled = false;
+  night.start = (req.body.start == 0)? 23 : parseInt(req.body.start);
+  night.end = (req.body.end == 0)? 7 : parseInt(req.body.end);
+  night.millis = (req.body.millis == 0)? 600000 : req.body.millis;
+  db.set("night", night).write();
+  fastmile.reloadNightTimer(db);
+  res.json({ "night": night });
 })
 
 app.get("/history", (req, res) => {
